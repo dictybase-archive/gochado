87a314f1a43089e4c863a3288bbf58304b4b2776
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-02-21 12:12:41 -0500

    Added tests for creating db and cvterm record

---
+++ chado_helper_test.go
@@ -0,0 +1,93 @@
+package gochado
+
+import (
+    "github.com/dictybase/testchado"
+    . "github.com/dictybase/testchado/matchers"
+    . "github.com/onsi/gomega"
+    "testing"
+)
+
+func TestFindOrCreateDbId(t *testing.T) {
+    RegisterTestingT(t)
+    chado := testchado.NewDBManager()
+    chado.DeploySchema()
+    _ = chado.LoadDefaultFixture()
+    defer chado.DropSchema()
+
+    helper := NewChadoHelper(chado.DBHandle())
+    dbid, err := helper.FindOrCreateDbId("gochado")
+    if err != nil {
+        t.Error(err)
+    }
+    dbid2, err2 := helper.FindOrCreateDbId("gochado")
+    if err2 != nil {
+        t.Error(err2)
+    }
+    if dbid != dbid2 {
+        t.Error("expected %d got %d", dbid, dbid2)
+    }
+}
+
+func TestFindOrCreateCvId(t *testing.T) {
+    RegisterTestingT(t)
+    chado := testchado.NewDBManager()
+    chado.DeploySchema()
+    _ = chado.LoadDefaultFixture()
+    defer chado.DropSchema()
+
+    helper := NewChadoHelper(chado.DBHandle())
+    cvid, err := helper.FindOrCreateCvId("sequence")
+    if err != nil {
+        t.Errorf("should have found cv sequence error: %s", err)
+    }
+    if cvid != 3 {
+        t.Error("should have matched the cv id")
+    }
+
+    cvid2, err2 := helper.FindOrCreateCvId("tc")
+    if err2 != nil {
+        t.Errorf("should have created cv tc error: %s", err2)
+    }
+    if cvid2 != 4 {
+        t.Errorf("Expected cvid 4 got %d", cvid2)
+    }
+}
+
+func TestFindOrCreateCvtermId(t *testing.T) {
+    RegisterTestingT(t)
+    chado := testchado.NewDBManager()
+    chado.DeploySchema()
+    _ = chado.LoadDefaultFixture()
+    defer chado.DropSchema()
+
+    helper := NewChadoHelper(chado.DBHandle())
+    id, err := helper.FindCvtermId("sequence", "gene")
+    if err != nil {
+        t.Errorf("should have found cvterm id: %s", err)
+    }
+
+    id2, err2 := helper.FindCvtermId("sequence", "gene")
+    if id != id2 {
+        t.Errorf("first id %d and second id %d should have matched: %s", id, id2, err2)
+    }
+
+    p := make(map[string]string)
+    p["cv"] = "gochado"
+    p["cvterm"] = "gochadoterm"
+    p["dbxref"] = "GC:59939"
+    _, err = helper.CreateCvtermId(p)
+    if err != nil {
+        t.Error(err)
+    }
+    Expect(chado).Should(HaveCvterm(p["cvterm"]))
+    Expect(chado).Should(HaveDbxref(p["dbxref"]))
+
+    p["cv"] = "seinfeld"
+    p["cvterm"] = "The chinese restaurant"
+    p["dbxref"] = "Todd Gag"
+    _, err = helper.CreateCvtermId(p)
+    if err != nil {
+        t.Error(err)
+    }
+    Expect(chado).Should(HaveDbxref("local:Todd Gag"))
+}
