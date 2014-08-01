a636aebb6f350c0fcef65f07802c740950ecf273
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-08-01 19:39:29 +0100

    Added gorm tag for primary key fields

--- model.go
+++ model.go
@@ -1,124 +1,124 @@
 package gochado
 
 import (
-    "time"
+	"time"
 )
 
 type Dbxref struct {
-    DbxrefId    int64 `primary_key:"dbxref_id"`
-    Accession   string
-    Version     string
-    Description string
-    //Foreign key
-    DbId int64
-    // Embedded struct belongs_to relation
-    Db  Db
-    //has_many relations
-    FeatureDbxrefs []FeatureDbxref
+	DbxrefId    int64 `gorm:"primary_key:yes"`
+	Accession   string
+	Version     string
+	Description string
+	//Foreign key
+	DbId int64
+	// Embedded struct belongs_to relation
+	Db Db
+	//has_many relations
+	FeatureDbxrefs []FeatureDbxref
 }
 
 type Db struct {
-    DbId        int64 `primary_key:"db_id"`
-    Name        string
-    Description string
-    Urlprefix   string
-    Url         string
-    // has_many relations
-    Dbxrefs []Dbxref
+	DbId        int64 `gorm:"primary_key:yes"`
+	Name        string
+	Description string
+	Urlprefix   string
+	Url         string
+	// has_many relations
+	Dbxrefs []Dbxref
 }
 
 type Cv struct {
-    CvId       int64 `primary_key:"cv_id"`
-    Name       string
-    Definition string
-    Cvterms    []Cvterm
+	CvId       int64 `gorm:"primary_key:yes"`
+	Name       string
+	Definition string
+	Cvterms    []Cvterm
 }
 
 type Cvterm struct {
-    CvtermId           int64 `primary_key:"cvterm_id"`
-    Name               string
-    Definition         string
-    IsObsolete         bool
-    IsRelationshiptype bool
+	CvtermId           int64 `gorm:"primary_key:yes"`
+	Name               string
+	Definition         string
+	IsObsolete         bool
+	IsRelationshiptype bool
 
-    //Foreign keys
-    CvId     int64
-    Cv       Cv
-    DbxrefId int64
-    Dbxref   Dbxref
+	//Foreign keys
+	CvId     int64
+	Cv       Cv
+	DbxrefId int64
+	Dbxref   Dbxref
 }
 
 type Organism struct {
-    OrganismId   int64 `primary_key:"organism_id"`
-    Abbreviation string
-    Genus        string
-    Species      string
-    CommonName   string
-    Comment      string
-    Features     []Feature
+	OrganismId   int64 `gorm:"primary_key:yes"`
+	Abbreviation string
+	Genus        string
+	Species      string
+	CommonName   string
+	Comment      string
+	Features     []Feature
 }
 
 type Feature struct {
-    FeatureId        int64 `primary_key:"feature_id"`
-    Name             string
-    Uniquename       string
-    Residues         string
-    Seqlen           int64
-    Md5checksum      string
-    IsAnalysis       bool
-    IsObsolete       bool
-    Timeaccessioned  time.Time
-    Timelastmodified time.Time
+	FeatureId        int64 `gorm:"primary_key:yes"`
+	Name             string
+	Uniquename       string
+	Residues         string
+	Seqlen           int64
+	Md5checksum      string
+	IsAnalysis       bool
+	IsObsolete       bool
+	Timeaccessioned  time.Time
+	Timelastmodified time.Time
 
-    //foreign keys
-    OrganismId int64
-    Organism   Organism
-    DbxrefId   int64
-    Dbxref     Dbxref
-    TypeId     int64
-    Type       Cvterm
+	//foreign keys
+	OrganismId int64
+	Organism   Organism
+	DbxrefId   int64
+	Dbxref     Dbxref
+	TypeId     int64
+	Type       Cvterm
 
-    //has_many relations
-    FeatureCvterms []FeatureCvterm
-    FeatureDbxrefs []FeatureDbxref
+	//has_many relations
+	FeatureCvterms []FeatureCvterm
+	FeatureDbxrefs []FeatureDbxref
 }
 
 type Pub struct {
-    PubId       int64 `primary_key:"pub_id"`
-    Title       string
-    Volumetitle string
-    Volume      string
-    SeriesName  string
-    Issue       string
-    Pyear       string
-    Pages       string
-    Miniref     string
-    Uniquename  string
-    Publisher   string
-    Pubplace    string
-    IsObsolete  bool
-    //foreign key
-    TypeId int64
-    Type   Cvterm
+	PubId       int64 `gorm:"primary_key:yes"`
+	Title       string
+	Volumetitle string
+	Volume      string
+	SeriesName  string
+	Issue       string
+	Pyear       string
+	Pages       string
+	Miniref     string
+	Uniquename  string
+	Publisher   string
+	Pubplace    string
+	IsObsolete  bool
+	//foreign key
+	TypeId int64
+	Type   Cvterm
 }
 
 type FeatureCvterm struct {
-    FeatureCvtermId int64 `primary_key:"feature_cvterm_id"`
-    IsNot           bool
-    Rank            int64
-    //foreign keys
-    FeatureId int64
-    Feature   Feature
-    CvtermId  int64
-    Cvterm    Cvterm
-    PubId     int64
-    Pub       Pub
+	FeatureCvtermId int64 `gorm:"primary_key:yes"`
+	IsNot           bool
+	Rank            int64
+	//foreign keys
+	FeatureId int64
+	Feature   Feature
+	CvtermId  int64
+	Cvterm    Cvterm
+	PubId     int64
+	Pub       Pub
 }
 
 type FeatureDbxref struct {
-    FeatureDbxrefId int64 `primary_key:"feature_dbxref_id"`
-    FeatureId       int64
-    DbxrefId        int64
-    Feature         Feature
-    Dbxref          Dbxref
+	FeatureDbxrefId int64 `gorm:primary_key:yes"`
+	FeatureId       int64
+	DbxrefId        int64
+	Feature         Feature
+	Dbxref          Dbxref
 }
