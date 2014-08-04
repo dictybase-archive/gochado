8c5af4ac2d6db4747e0ac668ddd7ddce0452b8ad
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-08-04 20:28:38 +0100

    Replaced non testing failures with log.Fatal calls

--- fixture_test.go
+++ fixture_test.go
@@ -2,6 +2,7 @@ package gochado
 
 import (
 	"encoding/gob"
+	"log"
 	"testing"
 
 	"github.com/GeertJohan/go.rice"
@@ -19,35 +20,35 @@ func TestGpadFixtureLoader(t *testing.T) {
 	r, err := b.Open("fixture.gob")
 	defer r.Close()
 	if err != nil {
-		t.Error("Could not get gob file fixture.gob")
+		log.Fatal("Could not get gob file fixture.gob")
 	}
 	// Now decode and get the data
 	dec := gob.NewDecoder(r)
 	var genes []string
 	err = dec.Decode(&genes)
 	if err != nil {
-		t.Error(err)
+		log.Fatal(err)
 	}
 	f := NewGpadFixtureLoader(chado)
 	g := f.LoadGenes(genes)
-	if len(g) != 5 {
-		t.Errorf("expected %d genes got %d", 5, len(g))
+	if len(g) != 7 {
+		log.Fatalf("expected %d genes got %d", 5, len(g))
 	}
 
 	var gorefs []string
 	err = dec.Decode(&gorefs)
 	if err != nil {
-		t.Error(err)
+		log.Fatal(err)
 	}
 	p := f.LoadPubIds(gorefs)
 	if len(p) != 3 {
-		t.Errorf("expected %d pubs got %d", 3, len(p))
+		log.Fatalf("expected %d pubs got %d", 3, len(p))
 	}
 
 	var goids map[string][]string
 	err = dec.Decode(&goids)
 	if err != nil {
-		t.Error(err)
+		log.Fatal(err)
 	}
 	goterm := f.LoadGoIds(goids)
 	if len(goterm) != 8 {
