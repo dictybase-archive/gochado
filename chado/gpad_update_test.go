ca7593be5adda9428df9210dfddd0c134c4f7b90
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-09-29 22:31:29 +0100

    Added tests to check for deletion of explicit and implicit columns of updated records

--- chado/gpad_update_test.go
+++ chado/gpad_update_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	. "github.com/dictybase/testchado/matchers"
+	"github.com/jmoiron/sqlx"
 	. "github.com/onsi/gomega"
 )
 
@@ -19,15 +20,16 @@ func TestGpadUpdateSqlite(t *testing.T) {
 	setup := setUpSqliteTest()
 	chado := setup.chado
 	dbh := chado.DBHandle()
-	sqlite := NewChadoSqlite(dbh, setup.parser, ont, acv, adb)
+	p := setup.parser
+	sqlite := NewChadoSqlite(dbh, p, ont, acv, adb)
 	// Get the changed(new/updated) entries in another staging tables
-	dbh.MustExec(setup.parser.GetSection("insert_latest_goa_from_staging"), ont)
+	dbh.MustExec(p.GetSection("insert_latest_goa_from_staging"), ont)
 	// Load the first test data in chado schema
 	sqlite.RunBulkInserts()
 	// Load the second test data(with updated entries) in staging schema
-	LoadUpdatedGpadStagingSqlite(dbh, setup.rice, setup.parser)
+	LoadUpdatedGpadStagingSqlite(dbh, setup.rice, p)
 	// Get the changed(new/updated) entries in another staging tables
-	dbh.MustExec(setup.parser.GetSection("insert_latest_goa_from_staging"), ont)
+	dbh.MustExec(p.GetSection("insert_latest_goa_from_staging"), ont)
 	// Tag the updatable records
 	sqlite.MarkUpdatable()
 	//Teardown
@@ -40,15 +42,27 @@ func TestGpadUpdateSqlite(t *testing.T) {
 	// Check the number of updatable entries
 	Expect("SELECT COUNT(*) FROM temp_gpad_new WHERE is_update = 1").Should(HaveCount(4))
 
-	// Now update the records, here we go by delete and insert
-	// It will be qualifier, withfrom and extra pubs
-	type tstr struct {
-		block string
-		count int
-	}
 	tbl := map[string]int{
-		"delete_feature_cvtermprop_qualifier": 3,
-		"delete_feature_cvtermprop_withfrom":  3,
-		"delete_feature_cvterm_pub":           0,
+		"delete_feature_cvtermprop_qualifier": 4,
+		"delete_feature_cvtermprop_withfrom":  4,
 	}
+	for k, v := range tbl {
+		runRegularGpadExpl(dbh, p.GetSection(k), v, ont)
+	}
+	runRegularGpadImpl(dbh, p.GetSection("delete_feature_cvterm_pub"), 0)
+}
+
+func runRegularGpadImpl(dbh *sqlx.DB, section string, expected int) {
+	res, err := dbh.Exec(section)
+	Expect(err).ShouldNot(HaveOccurred())
+	rc, err := res.RowsAffected()
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(int(rc)).Should(Equal(expected))
+}
+func runRegularGpadExpl(dbh *sqlx.DB, section string, expected int, cv string) {
+	res, err := dbh.Exec(section, cv)
+	Expect(err).ShouldNot(HaveOccurred())
+	rc, err := res.RowsAffected()
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(int(rc)).Should(Equal(expected))
 }
